f6925ac0470f2c3a3c5cc7f10d19e1971b88deee
Candy Garcia <candygarcia014@gmail.com>
2021-01-17 21:07:08 -0800

    adding questions index.js

--- index.js
+++ index.js
@@ -1,37 +1,142 @@
-const inquirer = require("inquirer");
+//All requires
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-// look at hw from the last employee management sysrtem to find questions/ functions to add 
-// department to view/ and update.  
+//Set up connection
+var connection = mysql.createConnection({
+    host: "localhost",
 
-What do you want to do ?
-        Dropdown: select menu
-    Once selected - view = show all /
+    // port 
+    port: 3306,
 
-    //Add = give the questions to fill in columns/
-    //add = 3  functions to add to each table dep/role/empl. 
+    // username
+    user: "root",
 
-    //update = which emp to update and how do you want to update it, giving them the questions to fill in columns. 
-    Employee 's manager = updating the manager id when updating options are given. 
+    // password
+    password: "password",
+    database: "employees_DB"
+});
 
+//Prompt question 1
+connection.connect(function(err) {
+    if (err) throw err;
+    askFirstQuestion();
+});
 
-
-
-foreign key = reference to another table.
-foreign Key = able to attach it to an ID and reference it within other tables.
-
-
-left join!!-asssociating the id# with the actual record of the repartment / dep name.
-ex: 1 = hr.2 = software.*look at sql and left join tutirials
-for this!
-
-    To search:
-    Schema: foreign keys
-constraints
-references
-indexes
-left join - slq to select columns from other tables.
-
-// get strated: create the question to get the user's operation and create the function to VIEW the department. 
-select all from department.
-Then move on to the dropdown menu besides viewing - look at the question types from inquiere.
-Inquiere.js library to see questions.
+//First Question: Select action
+function askFirstQuestion() {
+    inquirer
+        .prompt([{
+            name: "firstQuestion",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "View Departments",
+                "View Roles",
+                "View Employees",
+                "Update Employee Role",
+                "exit"
+            ]
+        }, {
+            name: "departmentName",
+            type: "input",
+            message: "What is the name of the department you'd like to add?",
+            when: function(answer) {
+                return !!answer.firstQuestion && (answer.firstQuestion === "Add Department")
+            }
+        }, {
+            name: "roleTitle",
+            type: "input",
+            message: "What is the title of the role you'd like to add?",
+            when: function(answer) {
+                return !!answer.firstQuestion && (answer.firstQuestion === "Add Role")
+            }
+        }, {
+            name: "roleSalary",
+            type: "input",
+            message: "What is the salary of the role you'd like to add?",
+            when: function(answer) {
+                return !!answer.firstQuestion && (answer.firstQuestion === "Add Role")
+            }
+        }, {
+            name: "roleDepartmentID",
+            type: "input",
+            message: "What is the department ID of the role you'd like to add?",
+            when: function(answer) {
+                return !!answer.firstQuestion && (answer.firstQuestion === "Add Role")
+            }
+        }, {
+            name: "firstName",
+            type: "input",
+            message: "What is the first name of the employee you'd like to add?",
+            when: function(answer) {
+                return !!answer.firstQuestion && (answer.firstQuestion === "Add Employee")
+            }
+        }, {
+            name: "lastName",
+            type: "input",
+            message: "What is the last name of the employee you'd like to add?",
+            when: function(answer) {
+                return !!answer.firstQuestion && (answer.firstQuestion === "Add Employee")
+            }
+        }, {
+            name: "employeeRoleID",
+            type: "input",
+            message: "What is the role ID of the employee you'd like to add?",
+            when: function(answer) {
+                return !!answer.firstQuestion && (answer.firstQuestion === "Add Employee")
+            }
+        }, {
+            name: "employeeManagerID",
+            type: "input",
+            message: "What is the manager ID of the employee you'd like to add?",
+            when: function(answer) {
+                return !!answer.firstQuestion && (answer.firstQuestion === "Add Employee")
+            }
+        }, {
+            name: "updateEmployeeID",
+            type: "input",
+            message: "What is the employee ID of the employee you'd like to update?",
+            when: function(answer) {
+                return !!answer.firstQuestion && (answer.firstQuestion === "Update Employee Role")
+            }
+        }, {
+            name: "updateEmployeeRole",
+            type: "input",
+            message: "What is the new role ID of the employee",
+            when: function(answer) {
+                return !!answer.firstQuestion && (answer.firstQuestion === "Update Employee Role")
+            }
+        }])
+        .then(function(answer) {
+            switch (answer.firstQuestion) {
+                case "Add Department":
+                    addOptions("department", answer);
+                    break;
+                case "Add Role":
+                    addOptions("role", answer);
+                    break;
+                case "Add Employee":
+                    addOptions("employee", answer);
+                    break;
+                case "View Departments":
+                    viewOptions("department", answer);
+                    break;
+                case "View Roles":
+                    viewOptions("role", answer);
+                    break;
+                case "View Employees":
+                    viewOptions("employee", answer);
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole(answer);
+                    break;
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
